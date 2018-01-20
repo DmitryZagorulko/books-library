56837cbecbe2cec2906fc1b5851a1bce351bdf7c
DmitryZagorulko <IntensoDZ@yandex.ru>
2018-01-21 02:47:36 +0300

    add bundle, controller, entity and set fos

---
+++ src/BookBundle/Controller/BookController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace BookBundle\Controller;
+
+use BookBundle\Entity\Book;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Book controller.
+ *
+ * @Route("/")
+ */
+class BookController extends Controller
+{
+    /**
+     * Lists all book entities.
+     *
+     * @Route("/", name="_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $books = $em->getRepository('BookBundle:Book')->findAll();
+
+        return $this->render('@Book/book/index.html.twig', array(
+            'books' => $books,
+        ));
+    }
+
+    /**
+     * Creates a new book entity.
+     *
+     * @Route("/new", name="_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $book = new Book();
+        $form = $this->createForm('BookBundle\Form\BookType', $book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($book);
+            $em->flush();
+
+            return $this->redirectToRoute('_show', array('id' => $book->getId()));
+        }
+
+        return $this->render('@Book/book/new.html.twig', array(
+            'book' => $book,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a book entity.
+     *
+     * @Route("/book/{id}", name="_show")
+     * @Method("GET")
+     */
+    public function showAction(Book $book)
+    {
+        $deleteForm = $this->createDeleteForm($book);
+
+        return $this->render('@Book/book/show.html.twig', array(
+            'book' => $book,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing book entity.
+     *
+     * @Route("/{id}/edit", name="_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Book $book)
+    {
+        $deleteForm = $this->createDeleteForm($book);
+        $editForm = $this->createForm('BookBundle\Form\BookType', $book);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('_edit', array('id' => $book->getId()));
+        }
+
+        return $this->render('@Book/book/edit.html.twig', array(
+            'book' => $book,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a book entity.
+     *
+     * @Route("/book/{id}", name="_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Book $book)
+    {
+        $form = $this->createDeleteForm($book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($book);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('_index');
+    }
+
+    /**
+     * Creates a form to delete a book entity.
+     *
+     * @param Book $book The book entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Book $book)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('_delete', array('id' => $book->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/BookBundle/Form/BookType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BookBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('author')->add('cover')->add('file')->add('readIt')->add('allowDownload');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BookBundle\Entity\Book'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'bookbundle_book';
+    }
+
+
+}
